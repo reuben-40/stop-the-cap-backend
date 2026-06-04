3341508d2b97471ca5c74df63c0f82fe3dcb3242
reuben_samba <sambareuben40@gmail.com>
2026-06-04 12:25:43 +0300

    remove reset route

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from Website import create_app
 from flask import request
-from Website import db
 
 app = create_app()
 
@@ -8,14 +7,6 @@ app = create_app()
 def handle_options():
     if request.method == "OPTIONS":
         return app.make_default_options_response()
-
-# ── Temporary reset route ─────────────────────────────────────────────────────
-@app.route("/reset-db")
-def reset_db():
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-    return "Database reset successfully!"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=False)
